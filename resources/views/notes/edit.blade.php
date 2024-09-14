3f609dabc3fb87baa445635818aa220f8bfd3b0a
Benjamin Heymann <benjamin@bretler.ch>
2024-09-14 14:10:42 +0200

    added edit feature to notes

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -1,20 +1,20 @@
 @extends('base')
 
-@section('title', 'Create a note')
+@section('title', 'Update')
 
 @section('content')
   <h1>Create a note</h1>
   <form method="post" action="">
     <div class="mb-2">
       <label for="title" class="form-label">Title</label>
-      <input type="text" class="form-control" id="title" name="title"  value={{ old('title') }}>
+      <input type="text" class="form-control" id="title" name="title"  value="{{ old('title', $post->title) }}">
       @error("title")
         <p class="text-danger">{{ $message }}</p>
       @enderror
     </div>
     <div class="mb-2">
       <label for="content" class="form-label">Content</label>
-      <textarea class="form-control" id="content" name="content">{{ old('content') }}</textarea>
+      <textarea class="form-control" id="content" name="content">{{ old('content', $post->content) }}</textarea>
         @error("content")
         <p class="text-danger">{{ $message }}</p>
       @enderror
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [\App\Http\Controllers\Blog::class, 'index'])->name('notes.index');
 Route::get('/article/{slug}', [\App\Http\Controllers\Blog::class, 'show'])->name('notes.show');
 
 Route::get('/new', [\App\Http\Controllers\Blog::class, 'create'])->name('notes.create');
 Route::post('/new',[\App\Http\Controllers\Blog::class, 'store'])->name('notes.store');
+
+Route::get('/article/{slug}/edit', [\App\Http\Controllers\Blog::class, 'edit'])->name('notes.edit');
+Route::post('/article/{slug}/edit', [\App\Http\Controllers\Blog::class, 'update'])->name('notes.update');
